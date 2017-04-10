8911ef22167e566c242da55802dbf6ccb4036a72
郭政維 <kuo0515@eeit>
2017-04-10 14:53:23 +0800

    test 9211 commit
    qq

--- GroupBuy/src/main/java/eeit9212/websocket/GroupSocket.java
+++ GroupBuy/src/main/java/eeit9212/websocket/GroupSocket.java
@@ -1,8 +1,50 @@
 package eeit9212.websocket;
 
+import java.io.IOException;
 
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
 
-//@ServerEndpoint("/echo")
+@ServerEndpoint("/groupsocket")
 public class GroupSocket {
 
+	//收到client訊息便執行此方法.
+	//印出client所發送的訊息,傳送訊息給client.
+	  @OnMessage
+	  public void onMessage(String message, Session session)
+	    throws IOException, InterruptedException {
+	   
+	    System.out.println("收到訊息Received: " + message);
+	   
+	    session.getBasicRemote().sendText("Server has been received message.");
+	   
+//	   int sentMessages = 1;
+//	    while(sentMessages < 4){
+//	      Thread.sleep(1000);
+//	      session.getBasicRemote().
+//	        sendText("Server is counting 1 to 3. Count: "
+//	          + sentMessages);
+//	      sentMessages++;
+//	    }
+	   
+//	    session.getBasicRemote().sendText("End");
+	  }
+	   
+	  @OnOpen//client開啟連接.
+	  public void onOpen() {
+	    System.out.println("Client connected");
+	  }
+	 
+	  @OnClose//client關閉連接.
+	  public void onClose() {
+	    System.out.println("Connection closed");
+	  }
+	
+	
+	
+	
+	
 }
